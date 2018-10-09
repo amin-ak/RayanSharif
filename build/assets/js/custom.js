f35861743c58d322472aca7b48bdc38591d70747
Amin Akmali <amin.ceranando@gmail.com>
2018-10-09 12:17:51 +0330

    Initial commit

---
+++ build/assets/js/custom.js
@@ -0,0 +1,17 @@
+ // start : variable
+var title;
+var content;
+// end : variable
+  function C_alert()
+  {
+    title = 'قسمت مربوطه';
+    content = 'در دست ساخت';
+    swal(title,content);
+  }
+
+  function login()
+  {
+    title = 'ورود';
+    content='در دست ساخت';
+    swal(title,content);
+  }
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var pug;
+var stylus;
+var typeScript;
+
+gulp.task("test",function (){
+	console.log("\n    gulp is ready! let's go... \n ");	
+});
+
+gulp.task("pug",function(){
+	console.log("\n    [  pug convert to html successfully ] \n ");	
+	
+});
